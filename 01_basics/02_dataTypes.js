cdd9572ef64c1c1803208c5fe6e9e78ed5594e05
Syed Ahsan Nawab <159931963+SyedAhsan24@users.noreply.github.com>
2025-02-03 15:32:38 +0000

    operation in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strick"; // treat all js code as a newer version 
+ //alert(3+3) // We are using node js, not browser
+ console.log( 3 
+    +
+     3); // code readability should be high
+
+     console.log("ahsan")
+
+     let name = "syed Ahsan"
+     let age = 19
+     let isloggedIn = false
+
+     // number => 2 to power 53
+     // bigint
+     //string => ""
+     // boolean => true/false
+     // null => standalone value 
+     // undefined =>
+     // Symbol => unique 
+     // object 
+     
+     console.log(typeof undefined); // undefined
+     console.log(typeof null); // object
+     
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33 abc"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
+
+// 33 => 33
+// "33 abc" NaN
+// true => 1; false =>0
+
+let isloggedIn = "ahsan"
+let booleanIsloggedIn = Boolean(isloggedIn)
+//console.log(booleanIsloggedIn);
+
+// 1 => true false => 0
+// "" => false
+// "ahsan" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);

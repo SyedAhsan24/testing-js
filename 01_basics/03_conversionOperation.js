0fca2a0df83f75f9d69a13b6fec1b7bcb5045d9a
Syed Ahsan Nawab <159931963+SyedAhsan24@users.noreply.github.com>
2025-02-04 13:17:05 +0000

    conversion operation and comparision data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,4 +20,38 @@ let booleanIsloggedIn = Boolean(isloggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+// ******************************** Operation *********************************************
+
+let value = 3
+let negvalue = -3
+console.log( negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str = "hello"
+let str2 = " ahsan"
+let str3 = str + str2;
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((2 + 3) * 5 % 7);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100 
+++gameCounter;
+console.log(gameCounter);
+ // prefix and postfix js mdn
+ 
---
+++ 01_basics/05_datatypesSummary.js
@@ -0,0 +1,38 @@
+// primative 
+
+// 7 Types : String, Number, Boolean, Undefined, null, symbol,bigInt
+
+const score = 100;
+const scorevalue = 100.0
+
+const isloggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 34561237890456456789n
+
+console.log(typeof bigNumber );
+// Array
+const hero = ["amir", "salman", "sharukh"];
+// object 
+
+const myObj ={
+    name:"ahsan",
+    age:"19",
+}
+// function 
+const myfunction = function(){
+    console.log("hello World");
+    
+}
+
+// reference (Non primative)
+
+// dataTypes document url = { https://262.ecma-international.org/5.1/#sec-11.4.3}
+
+
+

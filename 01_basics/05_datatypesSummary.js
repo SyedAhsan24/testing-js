2bf63a75f47f429445eef17e174576a38950139e
Syed Ahsan Nawab <159931963+SyedAhsan24@users.noreply.github.com>
2025-02-09 16:13:51 +0000

    strings method

--- 01_basics/05_datatypesSummary.js
+++ 01_basics/05_datatypesSummary.js
@@ -11,11 +11,11 @@ let userEmail;
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 const bigNumber = 34561237890456456789n
 
-console.log(typeof bigNumber );
+//console.log(typeof bigNumber );
 // Array
 const hero = ["amir", "salman", "sharukh"];
 // object 
@@ -30,9 +30,30 @@ const myfunction = function(){
     
 }
 
+
 // reference (Non primative)
 
 // dataTypes document url = { https://262.ecma-international.org/5.1/#sec-11.4.3}
 
 
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (premitive),  Heap (non-premitive)
+
+let myyoutube = "Ahsan code youtube"
+let anothername = myyoutube
+anothername = "Syed Code"
+console.log(myyoutube);
+console.log(anothername);
+
+let userone ={
+    eamil: "syed@gmail.com",
+    code:"1234"
+}
+let usertwo = userone
+
+usertwo.eamil ="code@gmail.com"
+console.log(userone.eamil);
+console.log(usertwo.eamil);
 
